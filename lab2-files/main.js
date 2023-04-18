16f7dd2b44d3136ab10ce3208113347b3139004c
Jlyu015 <jlyu015@github.com>
2023-04-17 18:23:25 -0700

    lab submission

--- lab2-files/main.js
+++ lab2-files/main.js
@@ -3,21 +3,59 @@ let gameBoard = ["", "", "",
                  "", "", "",
                  "", "", ""];
 
+let boardWins = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8],[3,6,9] [1,5,9], [3,5,7], ];
 let gameOver = false;
-
 let currentPlayer = "X";
+var scoreX = 0;
+var scoreO = 0;
+
+var displayPlayer = document.getElementsByClassName("display_player");
+var cells = document.getElementsByClassName("xo");
 
 
-
-displayPlayer = document.getElementsByClassName("display_player");
-cells = document.getElementsByClassName("xo");
-
-
-console.log(cells)
+console.log(cells);
 
 function cellClicked(cellNum) {
+    console.log(displayPlayer)
+    
+    
     console.log("cell " + cellNum +" was clicked.");
+    switchPlayer();
+
+
+
 }
 function switchPlayer() {
-    currentPlayer = currentPlayer === "X" ? "0": "X";
+    currentPlayer = currentPlayer == "X" ? "0": "X";
+    updateDisplayPlayer();
 }
+function updateDisplayPlayer() {
+    displayPlayer.textContent = currentPlayer;
+  }
+
+function checkGameOver() { 
+    for(var i = 0; i < gameBoard.length; i++) {
+        if(gameBoard[i] == "") { // if theres still cells that are empty
+            return false;
+        }
+        
+        
+    }
+}
+function newGame() {
+    console.log("clicked new game")
+    let gameBoard = ["", "", "",
+                 "", "", "",
+                 "", "", ""];
+
+    let gameOver = false;
+    let currentPlayer = "X";
+}
+
+function reset() {
+    console.log("clicked reset")
+    newGame();
+    scoreX = 0;
+    scoreO = 0;
+}
+

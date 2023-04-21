abff3a767d35eda4e8da639fd515b4b349da489d
Diep, Jason <jdiep16@go.pasadena.edu>
2023-04-21 15:24:56 -0700

    .

---
+++ lab2-files/main.js
@@ -0,0 +1,15 @@
+var guessed_so_far = [];
+var max_attempts = 9;
+
+function startGame(){
+
+}
+
+function renderGame(){
+    document.getElementById("one").innerHTML = 
+    document.onclick("one")
+}
+
+function checkWinner(){
+
+}
